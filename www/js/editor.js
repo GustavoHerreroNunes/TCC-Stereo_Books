dceca5cd75aec4244b6744568781aa0115f4a2f4
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-12-01 18:58:59 -0300

    Telas de Cadastro de Login (branch telas)

---
+++ www/js/editor.js
@@ -0,0 +1,45 @@
+var app = {
+        
+    // Application Constructor
+
+    initialize: function() {
+        console.log("Iniciando....");
+        document.addEventListener('deviceready', app.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        console.log("");
+        document.getElementById("btnCadastro").addEventListener("click",app.inserir);  
+    },
+
+    inserir: function(){
+        var db = firebase.firestore();
+
+        let crazaosocial = document.getElementById("txtRazaoSocial").value;
+        let cnomefantasia = document.getElementById("txtNomeFantasia").value;
+        let ccnpj = document.getElementById("txtCNPJ").value;
+        let ctelefone = document.getElementById("txtTelefone").value;
+        let cemail = document.getElementById("txtEmail").value;
+        let csenha = document.getElementById("txtSenha").value;
+        
+
+        db.collection("editores").add({
+            Razao_Social: crazaosocial,
+            Nome_Fantasia: cnomefantasia,
+            CNPJ: ccnpj,
+            Telefone: ctelefone,
+            Email: cemail, 
+            Senha: csenha
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            window.location.href = cordova.file.applicationDirectory + "telas-Cadastro/cadastro-Leitor-1.html";
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+    }  
+};
+
+app.initialize();
---
+++ www/js/leitor.js
@@ -0,0 +1,43 @@
+var app = {
+        
+    // Application Constructor
+
+    initialize: function() {
+        console.log("Iniciando....");
+        document.addEventListener('deviceready', app.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        console.log("");
+        document.getElementById("btnCadastro").addEventListener("click",app.inserir);  
+    },
+
+    inserir: function(){
+        var db = firebase.firestore();
+        
+
+        let cnome = document.getElementById("txtNome").value;
+        let cdatanascimento = document.getElementById("txtDataNascimento").value;
+        let cemail = document.getElementById("txtEmail").value;
+        let csenha = document.getElementById("txtSenha").value;
+       
+        
+
+        db.collection("leitores").add({
+            Nome: cnome,
+            Data_Nascimento: cdatanascimento,
+            Email: cemail,
+            Senha: csenha
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            window.location.href = cordova.file.applicationDirectory + "telas-Cadastro/cadastro-Leitor-1.html";
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+    }  
+};
+
+app.initialize();
