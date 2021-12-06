373f4dfb7019764008534f83fc0d51d23f3db55f
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-12-06 19:04:23 -0300

    Trazendo as categorias do banco

---
+++ www/js/selecionar_categoria.js
@@ -0,0 +1,80 @@
+var app = {
+
+    initialize: function() {
+        console.log("Iniciando....");
+        document.addEventListener('deviceready', app.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        console.log("Device Ready");
+        document.getElementById("btnAvancar").addEventListener("click",app.inserir);
+        app.carregar();
+    },
+
+    carregar: function(){
+        var db = firebase.firestore();
+        
+        var collectionCategorias = db.collection('categorias');
+        
+        var categoriasAdicionadas = 0;
+        var rowId = 1;
+        var categorias = [
+            { id: 0, nome: '', icon: { normal: '', hover: '' } }
+        ];
+        document.getElementById("categorias").innerHTML = "<div class='row gap-4' id='row." + rowId + "'> </div></br>";
+        
+        collectionCategorias.get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                categorias.push({ id: 0, nome: '', icon: { normal: '', hover: '' } });
+                
+                categorias[categoriasAdicionadas].id = doc.id;
+                categorias[categoriasAdicionadas].nome = doc.data().nome;
+                categorias[categoriasAdicionadas].icon = {
+                    normal: doc.data().icon,
+                    hover: doc.data().icon_hover
+                }
+
+                document.getElementById("row."+rowId).innerHTML += "<div class='col' id='col." + categorias[categoriasAdicionadas].id + "'>"
+                                                                    + "<input type='checkbox' class='btn-check' id='" + categorias[categoriasAdicionadas].id + "' autocomplete='off'>"
+                                                                    + "<label class='btn btn-outline-primary' for='" + categorias[categoriasAdicionadas].id + "'>"
+                                                                        + "<img class='d-grid gap-2 col-10 mx-auto' src='" + categorias[categoriasAdicionadas].icon.normal + "' alt='Stereo Books Logo' />"
+                                                                        + categorias[categoriasAdicionadas].nome
+                                                                    + "</label>"
+                                                                + "</div>";
+
+                categoriasAdicionadas++;
+                if((categoriasAdicionadas % 6) == 0){
+                    rowId++;
+                    document.getElementById("categorias").innerHTML += "<div class='row gap-4' id='row." + rowId + "'></div>";
+                }
+
+            });
+
+            console.log('[categorias]', categorias);
+
+            categorias.forEach((categoria) => {
+                var nextIcon = categoria.icon.hover;
+
+                document.getElementById(categoria.id).addEventListener("click", function(){
+                    console.log("col." + categoria.id + " clicado");
+                    document.getElementById("col."+categoria.id).children[1].children[0].src=nextIcon;
+                    if(nextIcon == categoria.icon.hover){
+                        nextIcon = categoria.icon.normal;
+                    }else{
+                        nextIcon = categoria.icon.hover;
+                    }
+                });
+            })
+        })
+        .catch((error) => {
+            console.log("Erro ao consultar documento: " + error);
+        })
+    }, 
+    
+    inserir: function(){
+
+    }
+};
+
+app.onDeviceReady();
