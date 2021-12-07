3cc3cc045ce24c8e3f01cc4f7c083b7b0c58d7a2
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-12-07 15:57:52 -0300

    Cadastro de categorias favoritas

--- www/js/selecionar_categoria.js
+++ www/js/selecionar_categoria.js
@@ -1,5 +1,7 @@
 var app = {
 
+    userId: '',
+
     initialize: function() {
         console.log("Iniciando....");
         document.addEventListener('deviceready', app.onDeviceReady.bind(this), false);
@@ -8,7 +10,16 @@ var app = {
     onDeviceReady: function() {
         console.log("Device Ready");
         document.getElementById("btnAvancar").addEventListener("click",app.inserir);
+        app.getUserId();
         app.carregar();
+    },
+
+    //Método para coletar id do usuário
+    getUserId: function(){
+        //Captando parâmetro user da url
+        var string_url = window.location.href;
+        var url = new URL(string_url);
+        app.userId = url.searchParams.get("user");
     },
 
     carregar: function(){
@@ -36,7 +47,7 @@ var app = {
                 }
 
                 document.getElementById("row."+rowId).innerHTML += "<div class='col' id='col." + categorias[categoriasAdicionadas].id + "'>"
-                                                                    + "<input type='checkbox' class='btn-check' id='" + categorias[categoriasAdicionadas].id + "' autocomplete='off'>"
+                                                                    + "<input type='checkbox' class='btn-check' name='btnCategoria' value='" + categorias[categoriasAdicionadas].nome + "' id='" + categorias[categoriasAdicionadas].id + "' autocomplete='off'>"
                                                                     + "<label class='btn btn-outline-primary' for='" + categorias[categoriasAdicionadas].id + "'>"
                                                                         + "<img class='d-grid gap-2 col-10 mx-auto' src='" + categorias[categoriasAdicionadas].icon.normal + "' alt='Stereo Books Logo' />"
                                                                         + categorias[categoriasAdicionadas].nome
@@ -73,8 +84,33 @@ var app = {
     }, 
     
     inserir: function(){
+        console.log("Cadastrando Categorias");
 
+        var db = firebase.firestore();
+
+        var btnCategoria = document.getElementsByName("btnCategoria");
+        var registCategoria = [];
+        var positionOnArray = 0;
+        for(var i = 0; i < btnCategoria.length; i++){
+            if(btnCategoria[i].checked){
+                registCategoria[positionOnArray] = btnCategoria[i].value;
+                positionOnArray++;
+            }
+        }
+        var leitores = db.collection("leitores").doc(app.userId);
+
+        return leitores.update({
+            categorias_favoritas: registCategoria
+        })
+        .then(() => {
+            console.log("Document successfully updated!");
+            window.location.href = "../biblioteca.html?user=" + app.userId;
+        })
+        .catch((error) => {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
     }
 };
 
-app.onDeviceReady();
+app.initialize();
